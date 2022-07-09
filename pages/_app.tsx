9893ec33d66a4f33e713876ed55d613ee023873d
Chandler Klein <kleinc16@gmail.com>
2022-07-09 17:46:24 -0500

    Replacing with Next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Chandler Klein - Software Engineer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"/>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
