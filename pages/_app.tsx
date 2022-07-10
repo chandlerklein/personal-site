cedbc9b756a7789753a495dd6a8932b7ecc155ab
Chandler Klein <kleinc16@gmail.com>
2022-07-09 20:04:23 -0500

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import Head from "next/head";
 
 config.autoAddCss = false
 
@@ -13,11 +13,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div>
       <Head>
         <title>Chandler Klein - Software Engineer</title>
+        <meta charSet='UTF-8'/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <meta name="description"
               content="Chandler Klein's portfolio site for being a professional software engineer and messing around."/>
         <link rel="icon" type="image/x-icon" href="/favicon.png"/>
       </Head>
+
       <Component {...pageProps} />
     </div>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,17 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
-      <Head lang="en">
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500&display=swap"
               rel="stylesheet"/>
       </Head>
       <body>
-      <Main/>
-      <NextScript/>
+        <Main/>
+        <NextScript/>
       </body>
     </Html>
   )
